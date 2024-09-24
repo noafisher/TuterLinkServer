bfb5d844d367e03a97853c4be68ec2dada17f69c
User <User@LAB2-18>
2024-09-24 12:59:50 +0300

    Add project files.

---
+++ TuterLinkServer/DataBaseCreation.sql
@@ -0,0 +1,36 @@
+﻿﻿Use master
+Go
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'TuterLink_DB')
+BEGIN
+    DROP DATABASE TuterLink_DB;
+END
+Go
+Create Database TuterLink_DB
+Go
+Use TuterLink_DB
+Go
+
+--יצירת טבלת משתמשים
+CREATE TABLE Users(
+    Email nvarchar(100) PRIMARY KEY ,
+    Pass nvarchar(25) ,
+    TypeID int,
+    FOREIGN KEY(TypeID) REFERENCES TypeUser(TypeID)
+    )
+
+--סוג משתמשים
+CREATE TABLE TypeUser(
+    TypeID int PRIMARY KEY,
+    TypeName nvarchar(50)
+    )
+
+-- תלמידים
+CREATE TABLE Students(
+    StudentID int PRIMARY KEY,
+    Email nvarchar(100),
+     FOREIGN KEY(Email) REFERENCES Users(Email),
+    FirstName nvarchar(50),
+    LastName nvarchar(50)
+    )
+
+
